24036743ae0f156ba14d537d32f9cf48bb09634c
furkanguzel <furkanguzel0@gmail.com>
2021-08-17 22:45:04 +0300

    Initial commit

---
+++ src/main/java/com/furkanguzel/restapiversioning/RestApiVersioningApplication.java
@@ -0,0 +1,13 @@
+package com.furkanguzel.restapiversioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiVersioningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiVersioningApplication.class, args);
+    }
+
+}
